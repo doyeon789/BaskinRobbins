759ef9c6fe3161007a121d2415de7490f82f1fbc
doyeon789 <143865473+doyeon789@users.noreply.github.com>
2024-09-01 23:51:26 +0900

    Update Main.java
    주석 추가

--- BaskinRobbins/src/StartPage/Main.java
+++ BaskinRobbins/src/StartPage/Main.java
@@ -4,13 +4,15 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        // 기본 프레임 및 패널 작성
         JFrame frame = new JFrame("Baskin Robbins");
         JPanel panel = new JPanel();
 
+        // 프레임, 패널 편집
         FrameWork.Frame(frame);
         FrameWork.Panel(panel,frame);
-
+        
+        // 리로드
         panel.revalidate();
         panel.repaint();
     }
