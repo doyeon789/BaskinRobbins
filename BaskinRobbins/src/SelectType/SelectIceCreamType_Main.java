23a528c3c022f99e57e186f8b436c12ac4a82ac1
doyeon789 <143865473+doyeon789@users.noreply.github.com>
2024-09-02 18:09:28 +0900

    Update SelectIceCreamType_Main.java
    주석 추가

--- BaskinRobbins/src/SelectType/SelectIceCreamType_Main.java
+++ BaskinRobbins/src/SelectType/SelectIceCreamType_Main.java
@@ -5,12 +5,15 @@ import javax.swing.*;
 public class SelectIceCreamType_Main {
     public static void SICT_main() {
 
+        // 기본 프레임 및 패널 작성
         JFrame frame = new JFrame("Baskin Robbins");
         JPanel panel = new JPanel();
 
+        // 프레임, 패널 편집
         SICT_FrameWork.Frame(frame);
         SICT_FrameWork.Panel(panel,frame);
 
+        // 리로드
         panel.revalidate();
         panel.repaint();
     }
