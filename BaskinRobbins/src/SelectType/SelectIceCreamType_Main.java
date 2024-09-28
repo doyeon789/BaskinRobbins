93ef3e7971c8c63f19ffeea1e12fd1e4620ea6c5
doyeon789 <143865473+doyeon789@users.noreply.github.com>
2024-09-28 23:08:36 +0900

    Update SelectIceCreamType_Main.java

--- BaskinRobbins/src/SelectType/SelectIceCreamType_Main.java
+++ BaskinRobbins/src/SelectType/SelectIceCreamType_Main.java
@@ -1,35 +1,27 @@
 package SelectType;
 
+import StartPage.BaskinRobbins_Main;
+
 import javax.swing.*;
-import java.io.IOException;
 
 public class SelectIceCreamType_Main {
-    static JFrame frame;
-    static JPanel panel;
+    static JPanel panel2;
 
-    public static void SICT_main() throws IOException {
-        // 기본 프레임 및 패널 작성
-        frame = new JFrame("Baskin Robbins");
-        panel = new JPanel();
+    public static void SICT_main()  {
+
+        JFrame frame = BaskinRobbins_Main.getFrame();
+        panel2 = new JPanel();
 
         // 프레임, 패널 편집
-        SICT_FrameWork.Frame(frame);
-        SICT_FrameWork.Panel(panel,frame);
+        SICT_FrameWork.Panel(panel2,frame);
 
         // 리로드
-        panel.revalidate();
-        panel.repaint();
+        panel2.revalidate();
+        panel2.repaint();
     }
 
-    // 다른 클래스에 이 프레임을 전달하기 위한 함수
-    public static JFrame getFrame() {
-        return frame;
+    // 다른 클래스에 이 패널을 전달하기 위한 함수
+    public static JPanel getPanel2(){
+        return panel2;
     }
-
 }
-
-
-
-
-
-
