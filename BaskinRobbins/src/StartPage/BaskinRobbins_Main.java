b81b6da998ec2d896eb02d2d8f4ac5a7c1c56b1f
doyeon789 <143865473+doyeon789@users.noreply.github.com>
2024-09-28 23:07:24 +0900

    Update BaskinRobbins_Main.java

--- BaskinRobbins/src/StartPage/BaskinRobbins_Main.java
+++ BaskinRobbins/src/StartPage/BaskinRobbins_Main.java
@@ -4,24 +4,28 @@ import javax.swing.*;
 
 public class BaskinRobbins_Main {
     static JFrame frame;
-    static JPanel panel;
+    static JPanel panel1;
 
     public BaskinRobbins_Main(){
         // 기본 프레임 및 패널 작성
         frame = new JFrame("Baskin Robbins");
-        panel = new JPanel();
+        panel1 = new JPanel();
 
         // 프레임, 패널 편집
         FrameWork.Frame(frame);
-        FrameWork.Panel(panel,frame);
+        FrameWork.Panel(panel1,frame);
 
         // 리로드
-        panel.revalidate();
-        panel.repaint();
+        panel1.revalidate();
+        panel1.repaint();
     }
 
     // 다른 클래스에 이 프레임을 전달하기 위한 함수
     public static JFrame getFrame() {
         return frame;
+    }
+    // 다른 클래스에 이 패널을 전달하기 위한 함수
+    public static JPanel getPanel1() {
+        return panel1;
     }
 }
